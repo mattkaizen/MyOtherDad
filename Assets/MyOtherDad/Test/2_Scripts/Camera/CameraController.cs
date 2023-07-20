5b556e6cfc04a5329fa32964e9a7cc9f6270dff2
MatiasF <83260164+mattkaizen@users.noreply.github.com>
2023-07-20 12:32:46 -0300

    Added CameraController

---
+++ Assets/MyOtherDad/Test/2_Scripts/Camera/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using Cinemachine;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Camera
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private CinemachineVirtualCamera currentCamera;
+        [SerializeField] private Vector3 newRotation;
+
+        private void Awake()
+        {
+            ChangeRotation();
+            
+            currentCamera.m_Transitions.m_OnCameraLive.AddListener(OnCameraLive);
+        }
+
+        private void OnCameraLive(ICinemachineCamera arg0, ICinemachineCamera arg1)
+        {
+            ChangeRotation();
+        }
+
+        [UsedImplicitly]
+        public void ChangeRotation()
+        {
+            Quaternion rotation = Quaternion.Euler(newRotation);
+            currentCamera.ForceCameraPosition(currentCamera.transform.position, rotation);
+        }
+    }
+}
