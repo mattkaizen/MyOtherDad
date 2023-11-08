d767cb96d294feda8e45d0cf35967a204251b363
MatiasF <83260164+mattkaizen@users.noreply.github.com>
2023-11-08 20:50:24 -0300

    Movi el escritorio de lugar

---
+++ Assets/MyOtherDad/Test/2_Scripts/PointerGesture/PointerGesture.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace PointerGesture
+{
+    public class PointerGesture : MonoBehaviour
+    {
+        public RectTransform RectTransform
+        {
+            get => rectTransform;
+            set => rectTransform = value;
+        }
+        
+        [SerializeField] private RectTransform rectTransform;
+        [SerializeField] private List<RectTransform> pointsToSpawnGesture = new List<RectTransform>();
+
+        public Vector2 GetRandomSpawnPosition()
+        {
+            int randomIndex = Random.Range(0, pointsToSpawnGesture.Count);
+
+            return pointsToSpawnGesture[randomIndex].anchoredPosition;
+        }
+    }
+}
