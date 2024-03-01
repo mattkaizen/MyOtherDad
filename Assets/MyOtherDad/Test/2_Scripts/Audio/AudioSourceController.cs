30c1087e2513a46ce99bffead094fa917689017d
MatiasF <83260164+mattkaizen@users.noreply.github.com>
2024-03-01 00:58:45 -0300

    Added the voices sounds to assets folder

---
+++ Assets/MyOtherDad/Test/2_Scripts/Audio/AudioSourceController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using DG.Tweening;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Audio
+{
+    public class AudioSourceController : MonoBehaviour
+    {
+        [SerializeField] private AudioSource audioSourceToControl;
+        [Header("Fade settings")]
+        [SerializeField] private bool getMaxVolumeFromAudioSource = true;
+        [SerializeField] private float maxVolume;
+
+        private void Awake()
+        {
+            if (!getMaxVolumeFromAudioSource) return;
+
+            maxVolume = audioSourceToControl.volume;
+        }
+
+        [UsedImplicitly]
+        public void Play()
+        {
+            if (audioSourceToControl == null)
+            {
+                Debug.LogWarning($"Empty {typeof(AudioSource)} ");
+                return;
+            }
+
+            audioSourceToControl.Play();
+        }
+
+        [UsedImplicitly]
+        public void Stop()
+        {
+            if (audioSourceToControl == null)
+            {
+                Debug.LogWarning($"Empty {typeof(AudioSource)} ");
+                return;
+            }
+
+            audioSourceToControl.Stop();
+        }
+
+        [UsedImplicitly]
+        public void SetVolume(float newVolume)
+        {
+            if (audioSourceToControl == null)
+            {
+                Debug.LogWarning($"Empty {typeof(AudioSource)} ");
+                return;
+            }
+
+            audioSourceToControl.volume = newVolume;
+        }
+
+        [UsedImplicitly]
+        public void FadeInToMaxVolume(float fadeInDuration)
+        {
+            if (audioSourceToControl == null)
+            {
+                Debug.LogWarning($"Empty {typeof(AudioSource)} ");
+                return;
+            }
+
+            audioSourceToControl.DOFade(maxVolume, fadeInDuration);
+        }
+
+        [UsedImplicitly]
+        public void FadeOutVolume(float fadeInDuration)
+        {
+            if (audioSourceToControl == null)
+            {
+                Debug.LogWarning($"Empty {typeof(AudioSource)} ");
+                return;
+            }
+
+            audioSourceToControl.DOFade(0.0f, fadeInDuration);
+        }
+
+        [UsedImplicitly]
+        public void FadeOutVolumeAndStop(float fadeInDuration)
+        {
+            if (audioSourceToControl == null)
+            {
+                Debug.LogWarning($"Empty {typeof(AudioSource)} ");
+                return;
+            }
+
+            audioSourceToControl.DOFade(0.0f, fadeInDuration).OnComplete(Stop);
+        }
+    }
+}
