4e8a275d1b77895c57f153a943b21d39446ffcef
MatiasF <83260164+mattkaizen@users.noreply.github.com>
2024-03-05 01:42:25 -0300

    Added Task Panel

---
+++ Assets/MyOtherDad/Test/2_Scripts/Tasks/TaskDisplay.cs
@@ -0,0 +1,17 @@
+﻿using JetBrains.Annotations;
+using TMPro;
+using UnityEngine;
+
+namespace Tasks
+{
+    public class TaskDisplay : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text text;
+
+        [UsedImplicitly]
+        public void StrikeThroughText()
+        {
+            text.fontStyle = FontStyles.Strikethrough | FontStyles.Bold;
+        }
+    }
+}
