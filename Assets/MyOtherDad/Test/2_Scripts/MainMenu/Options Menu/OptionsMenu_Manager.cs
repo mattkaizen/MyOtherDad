ef081e782995a92295fe656d8ff7a930e133c308
Jesus Antonio Garcia Fusi <83846459+SilentCry1234@users.noreply.github.com>
2024-03-08 00:06:27 -0300

    Uploading changes to the "main menu" scene and uploading the first psychiatric memory

--- Assets/MyOtherDad/Test/2_Scripts/MainMenu/Options Menu/OptionsMenu_Manager.cs
+++ Assets/MyOtherDad/Test/2_Scripts/MainMenu/Options Menu/OptionsMenu_Manager.cs
@@ -1,11 +1,14 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using TMPro;
+using System;
 
 public class OptionsMenu_Manager : MonoBehaviour
 {
+    public TMP_Dropdown resolutionsDropdown;
+
     [SerializeField] GameObject optionsMenu;
     [SerializeField] GameObject credits;
 
@@ -14,9 +17,11 @@ public class OptionsMenu_Manager : MonoBehaviour
 
     [SerializeField] AudioMixer masterMixer;
 
-    [Range (-20, 20)]
+    [SerializeField] Resolution[] resolutions;
+
+    [Range (0, 100)]
     private float ambientVol;
-    [Range (-20, 20)]
+    [Range (0, 100)]
     private float sfxVol;
 
     public void TurnOffOptions()
@@ -34,30 +39,69 @@ public class OptionsMenu_Manager : MonoBehaviour
         credits.SetActive(false);
     }
 
-    void Start()
-    {
-        sliderAmbient.value = ambientVol;
-        sliderSFX.value = sfxVol;
-
-        sliderAmbient.minValue = -100;
-        sliderAmbient.maxValue = 100;
-
-        sliderSFX.minValue = -100;
-        sliderSFX.maxValue = 100;
-    }
-
-    void Update()
-    {
-        AmbientVolume();
-    }
-
     public void AmbientVolume()
     {
         masterMixer.SetFloat("AmbientVolume", Mathf.Log10(sliderAmbient.value) * 20);
     }
 
-    public void SfxVolume() 
+    public void SfxVolume()
     {
-        masterMixer.SetFloat("SFXVolume", Mathf.Log10(sliderSFX.value) * 20); 
+        masterMixer.SetFloat("SFXVolume", Mathf.Log10(sliderSFX.value) * 20);
+    }
+
+    public void ChangeResoltions(int resolutionsIndex)
+    {
+        PlayerPrefs.SetInt("resolutionNumber", resolutionsDropdown.value); 
+
+        Resolution resolution = resolutions[resolutionsIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    void Start()
+    {
+        sliderAmbient.value = ambientVol;
+        sliderSFX.value = sfxVol;
+
+        sliderAmbient.minValue = 0;
+        sliderAmbient.maxValue = 100;
+
+        sliderSFX.minValue = 0;
+        sliderSFX.maxValue = 100;
+
+        CheckResoltion();
+    }
+
+    void Update()
+    {
+        //AmbientVolume();
+        //SfxVolume();
+    }
+
+    private void CheckResoltion()
+    {
+        resolutions = Screen.resolutions;
+        resolutionsDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int resolucionActual = 0;
+
+        for (int i = 0; i < resolutions.Length; i++) 
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (Screen.fullScreen && resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                resolucionActual = i; 
+            }
+        }
+
+        resolutionsDropdown.AddOptions(options);
+        resolutionsDropdown.value = resolucionActual;
+        resolutionsDropdown.RefreshShownValue();
+
+        resolutionsDropdown.value = PlayerPrefs.GetInt("resolutionNumber", 0); 
     }
 }
---
+++ Assets/MyOtherDad/Test/2_Scripts/Videos/NextScene.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    [SerializeField] int sceneIndex;
+    [SerializeField] float maxTime;
+
+    private float timer;
+
+    void Start()
+    {
+        timer = Time.realtimeSinceStartup;
+    }
+
+    void Update()
+    {
+        if (timer < maxTime)
+        {
+            timer += Time.deltaTime;
+        }
+        else if (timer > maxTime) 
+        {
+            SceneManager.LoadScene(sceneIndex);
+            timer = 0;
+        }
+    }
+}
